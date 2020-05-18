413136eb90b608adeb39f1272aa6839f17d80493
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-05-18 15:55:46 +0900

    수정

--- app/src/main/java/com/example/todak/Intro/self_diagnosis_lung4.java
+++ app/src/main/java/com/example/todak/Intro/self_diagnosis_lung4.java
@@ -2,7 +2,10 @@ package com.example.todak.Intro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.todak.R;
 
@@ -12,5 +15,16 @@ public class self_diagnosis_lung4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_diagnosis_lung4);
+
+        Button yes = (Button)findViewById(R.id.yes_button);
+        Button no = (Button)findViewById(R.id.no_button);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung4.this, self_diagnosis_lung5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
